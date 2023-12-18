d554d46e0835ba7ba944724d3b667728f65afe3f
bushraaslam12345 <153254202+bushraaslam12345@users.noreply.github.com>
2023-12-17 18:52:30 -0800

    Create index.ts

---
+++ index.ts
@@ -0,0 +1,24 @@
+// Array
+// two methods to specify type of an Array
+// number[] ,string[] and so on
+// Array<number> ,Array<string> and so on
+
+
+// method 1
+
+let array1 : number[] = [1,2,3,4,5,6,7]; 
+console.log(array1[2]);
+
+// method 1 by string
+
+let array2 : string[] = ["apple","mango","pineapple","lemon"];
+console.log(array2[2]);
+
+// method 2
+
+let array3 :Array<number>= [1,2,3,4,5]
+console.log(array3[0]);
+
+// method to define empty array
+
+let array4 : number[] = [];
